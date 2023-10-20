3af070252f98cda46246e8b37a4c62cd63c2d6f7
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-20 19:02:33 +0500

    Create necessary models

---
+++ src/FileExplorer.ServerApp/FileExplorer.Application/FileStorage/Models/Storage/StorageDrive.cs
@@ -0,0 +1,22 @@
+﻿namespace FileExplorer.Application.FileStorage.Models.Storage;
+
+public class StorageDrive : IStorageEntry
+{
+    public required string Name { get; set; }
+
+    public required string Path { get; set; }
+
+    public required string Label { get; set; }
+
+    public required string Format { get; set; }
+
+    public required string Type { get; set; }
+
+    public long TotalSpace { get; set; }
+
+    public long AvailableSpace { get; set; }
+
+    public long UsedSpace { get; set; }
+
+    public StorageItemType ItemType { get; set; } = StorageItemType.Drive;
+}
---
+++ src/FileExplorer.ServerApp/FileExplorer.Application/FileStorage/Models/Storage/StorageItemType.cs
@@ -0,0 +1,8 @@
+﻿namespace FileExplorer.Application.FileStorage.Models.Storage;
+
+public enum StorageItemType
+{
+    Drive = 0,
+    Directory = 1,
+    File = 2
+}
