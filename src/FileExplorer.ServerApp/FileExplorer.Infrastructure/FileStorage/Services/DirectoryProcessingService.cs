ef6cb09050545136ab51d7e4f93dcdd9b5471997
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-20 19:04:45 +0500

    Create Directory Processing Service and Directories Controller

---
+++ src/FileExplorer.ServerApp/FileExplorer.Infrastructure/FileStorage/Services/DirectoryProcessingService.cs
@@ -0,0 +1,26 @@
+﻿using FileExplorer.Application.FileStorage.Models.Storage;
+using FileExplorer.Application.FileStorage.Services;
+
+namespace FileExplorer.Infrastructure.FileStorage.Services;
+
+public class DirectoryProcessingService : IDirectoryProcessingService
+{
+    private readonly IDirectoryService _directoryService;
+    private readonly IFileService _fileService;
+
+    public DirectoryProcessingService(IDirectoryService directoryService, IFileService fileService)
+    {
+        _directoryService = directoryService;
+        _fileService = fileService;
+    }
+
+    public ValueTask<List<IStorageEntry>> GetEntriesAsync(string directoryPath)
+    {
+        var storageItems = new List<IStorageEntry>();
+
+        storageItems.AddRange(_directoryService.GetSubDirectories(directoryPath));
+        storageItems.AddRange(_directoryService.GetFiles(directoryPath));
+
+        return new ValueTask<List<IStorageEntry>>(storageItems);
+    }
+}
