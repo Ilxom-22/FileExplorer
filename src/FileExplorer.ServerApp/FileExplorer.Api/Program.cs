f8e83994e0f03b0e1ecc6c5dfaaa811346da9826
Ilxom <ilxomkarimjonov22@gmail.com>
2023-10-20 19:01:50 +0500

    Install newtonsoft package to all projects

--- src/FileExplorer.ServerApp/FileExplorer.Api/Program.cs
+++ src/FileExplorer.ServerApp/FileExplorer.Api/Program.cs
@@ -1,24 +1,30 @@
+using FileExplorer.Application.FileStorage.Brokers;
+using FileExplorer.Application.FileStorage.Services;
+using FileExplorer.Infrastructure.FileStorage.Brokers;
+using FileExplorer.Infrastructure.FileStorage.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddControllers().AddNewtonsoftJson();
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IDirectoryProcessingService, DirectoryProcessingService>();
+builder.Services.AddScoped<IDirectoryService, DirectoryService>();
+builder.Services.AddScoped<IFileService, FileService>();
+builder.Services.AddScoped<IDirectoryBroker, DirectoryBroker>();
+builder.Services.AddScoped<IFileBroker, FileBroker>();
+builder.Services.AddScoped<IDriveBroker, DriveBroker>();
+builder.Services.AddScoped<IDriveService, DriveService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
 
-app.UseAuthorization();
+app.UseStaticFiles();
 
 app.MapControllers();
 
